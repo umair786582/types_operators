8d70225b1c39a27ed210145b08c34b65e7835ab3
umair786582 <160109939+umair786582@users.noreply.github.com>
2024-03-10 23:32:32 +0500

    Add files via upload
    its my first Arithmetic operators project with the help of most experience teacher and on typescript

---
+++ main.ts
@@ -0,0 +1,74 @@
+// Addition
+
+let plus = 7;
+let add = 10
+let answer = (plus + add);
+console.log(answer)
+
+// Substractions
+
+let minus = 10;
+let value = 3;
+let ans = (minus - value);
+console.log(ans)
+
+// Multiplication
+
+let multi = 2;
+let multi1 = 10;
+let sum =(multi * multi1);
+console.log(sum)
+
+// Division
+
+let devi = 100
+let devi1 = 3
+let sum1 =(devi / devi1)
+console.log(sum1)
+
+// Exponential
+
+let expo = 2;
+let expo1 = 3;
+let sum2 =(expo ** expo1);
+console.log (sum2)
+
+// Modulus
+
+let per = 10;
+let per1 = 3;
+let percentage =(per % per1);
+console.log(percentage)
+
+//Unary operators Postfix
+
+let a = 7;
+console.log(a++)
+console.log(a)
+
+let guest = 5;
+console.log(guest--)
+console.log(guest)
+
+// Prefis Operator
+
+let increse = 10;
+console.log(++increse);
+
+
+let u = 5;
+let m = 2;
+let c= ++u + u++ + --m + m-- + u + m++ + m;
+console.log(c)
+
+
+let result = 3 + 4 * 5;
+console.log(result);
+
+let multiply = 3 + 4 * 5;
+multiply ++
+console.log(multiply);
+
+
+
+
